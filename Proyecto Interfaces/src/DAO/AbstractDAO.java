4e96eb8a53a81801135b55e039497ac1d433d6c7
Roque1211 <roque1211@gmail.com>
2020-10-27 15:49:51 +0100

    Abstract DAO con conexión

--- Proyecto Interfaces/src/DAO/AbstractDAO.java
+++ Proyecto Interfaces/src/DAO/AbstractDAO.java
@@ -1,5 +1,34 @@
 package DAO;
 
-public class AbstractDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import Common.Constantes;
+
+public abstract class AbstractDAO {
+
+    // conexión a la BD
+    public static Connection conexion = null;
+	/**
+	 * Define el objeto para conectar con una BD MySQL
+	 * @return retorna un objeto connection
+	 */
+	public void conectar() {
+
+        try {
+           Class.forName(Constantes.CONTROLADOR);
+            conexion = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.CLAVE);
+            System.out.println("Conexión OK");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al cargar el controlador");
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión");
+            e.printStackTrace();
+        }
+
+    }
 }
--- Proyecto Interfaces/src/Models/Usuarios.java
+++ Proyecto Interfaces/src/Models/Usuarios.java
@@ -1,5 +1,7 @@
 package Models;
 
 public class Usuarios {
-
+/*
+ * Sólo está creado para poder hacer el push sin problemas
+ */
 }
